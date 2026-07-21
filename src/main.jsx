9dc4a9b912d002d92cc62682775db730624aac3b
callavi <avishreeroy97@gmail.com>
2026-07-21 19:03:40 +0530

    chore: establish global styling foundation

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./styles/reset.css";
+import "./styles/tokens.css";
+import "./styles/globals.css";
 import './index.css'
 import App from './App.jsx'
 
